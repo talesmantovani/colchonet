d327126397b9fe420e773559b31c8b58558b3088
Tales Mantovani <tales.mantovani@spider.ad>
2015-12-04 02:03:16 -0200

    Annotate fields

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,14 @@
 class Room < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  location    :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  
+
   attr_reader :bio, :email, :full_name, :location, :password
-  
+
   #Validates
   validates :email, :presence => true,
                     :format => { :with => /\A[^@]+@[^@\.]+\z/ },
@@ -9,5 +9,23 @@ class User < ActiveRecord::Base
   validates :email, :full_name, :location, :presence => true
   validates_confirmation_of :password
   validates_length_of :bio, :minimum => 30, :allow_blank => false
-  
+
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  full_name  :string
+#  email      :string
+#  password   :string
+#  location   :string
+#  bio        :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
